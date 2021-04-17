9501d4b7a20688a461294db558b9e595d807e09d
Oyanna <moonlightreprise@gmail.com>
2021-04-17 13:33:40 +0600

    w4

---
+++ lesson4/5.py
@@ -0,0 +1,10 @@
+eda = [ "беш", "плов", "торт"]
+for food in eda:
+   if food == "манты":
+       print("Я не ем манты")
+       break
+       print("Вкусные " + food)
+   else:
+       print("Хорошо, что я не ем торт")
+
+print("Ужин окончен")
